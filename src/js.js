c3fbca5c2f4e9c2407315442e06bde935b7c7e43
saroltah <saroltah@users.noreply.github.com>
2022-11-23 21:50:28 +0100

    added api geolocation

--- src/js.js
+++ src/js.js
@@ -1,13 +1,19 @@
-let apiKey = "1fa04c70c5487af6b7c48dd7dfcb0b3f";
-let cityInput = "Copenhagen";
-let units = "metric";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=${units}`;
-axios.get(apiURL).then(showTemperature);
-function showTemperature(response) {
-  let currentCityName = document.querySelector("#current-city-name");
-  let currentDegree = document.querySelector("#current-degree");
-  let currentCondition = document.querySelector("#current-condition");
-  currentCityName.innerHTML = response.data.name;
-  currentDegree.innerHTML = Math.round(response.data.main.temp);
-  currentCondition.innerHTML = response.data.weather[0].main;
+navigator.geolocation.getCurrentPosition(showPlace);
+function showPlace(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+
+  let apiKey = "1fa04c70c5487af6b7c48dd7dfcb0b3f";
+  let cityInput = "Copenhagen";
+  let units = "metric";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  axios.get(apiURL).then(showTemperature);
+  function showTemperature(response) {
+    let currentCityName = document.querySelector("#current-city-name");
+    let currentDegree = document.querySelector("#current-degree");
+    let currentCondition = document.querySelector("#current-condition");
+    currentCityName.innerHTML = response.data.name;
+    currentDegree.innerHTML = Math.round(response.data.main.temp);
+    currentCondition.innerHTML = response.data.weather[0].main;
+  }
 }
