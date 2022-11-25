70a833aec9a8288c01c29a98d226bf6436b8425a
saroltah <saroltah@users.noreply.github.com>
2022-11-25 19:43:50 +0100

    made search engine to work

--- src/js.js
+++ src/js.js
@@ -44,3 +44,34 @@ let days = [
 currentDay.innerHTML = days[now.getDay()];
 let currentHour = document.querySelector("#current-hour");
 currentHour.innerHTML = `${now.getHours()} : ${now.getMinutes()}`;
+
+let searchCity = document.querySelector("#search-city");
+searchCity.addEventListener("submit", chooseCity);
+function chooseCity(event) {
+  event.preventDefault();
+  let apiKey = "1fa04c70c5487af6b7c48dd7dfcb0b3f";
+  let cityInput = document.querySelector("#type-city");
+  let units = "metric";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${units}`;
+  axios.get(apiURL).then(showTemperature);
+  function showTemperature(response) {
+    let currentCityName = document.querySelector("#current-city-name");
+    let currentDegree = document.querySelector("#current-degree");
+    let currentCondition = document.querySelector("#current-condition");
+    let currentWind = document.querySelector("#current-wind");
+    let currentHumid = document.querySelector("#current-humid");
+    currentCityName.innerHTML = response.data.name;
+    currentDegree.innerHTML = Math.round(response.data.main.temp);
+    currentCondition.innerHTML = response.data.weather[0].main;
+    currentWind.innerHTML = `Wind: ${Math.round(
+      response.data.wind.speed
+    )} km/h`;
+    currentHumid.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+    let CurrentEmoji = document.querySelector(`#current-emoji`);
+    CurrentEmoji.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    CurrentEmoji.setAttribute("alt", `${response.data.weather[0].description}`);
+  }
+}
