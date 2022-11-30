be8d01e0877fde91498a4cdc1d158b1efb4bfaab
saroltah <saroltah@users.noreply.github.com>
2022-11-30 20:56:13 +0100

    united opening page's and search button's function

--- src/js.js
+++ src/js.js
@@ -12,6 +12,7 @@ let changeCelcius = document.querySelector("#celsius");
 let changeFharenheit = document.querySelector("#fharenheit");
 
 navigator.geolocation.getCurrentPosition(showPlace);
+
 function showPlace(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
@@ -20,6 +21,7 @@ function showPlace(position) {
 
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
   axios.get(apiURL).then(showTemperature);
+
   function showTemperature(response) {
     currentCityName.innerHTML = response.data.name;
     currentDegree.innerHTML = Math.round(response.data.main.temp);
@@ -34,79 +36,20 @@ function showPlace(position) {
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
     CurrentEmoji.setAttribute("alt", `${response.data.weather[0].description}`);
+
+    //searchCity
+
+    let searchCity = document.querySelector("#search-city");
+    searchCity.addEventListener("submit", chooseCity);
+
+    function chooseCity(event) {
+      event.preventDefault();
+      let cityInput = document.querySelector("#type-city");
+      let searchURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${units}`;
+      axios.get(searchURL).then(showTemperature);
+    }
 
     //celsius to fharenheit
-
-    changeFharenheit.classList.add("link-like");
-
-    changeCelcius.addEventListener("click", convertCelsius);
-    function convertCelsius(event) {
-      event.preventDefault();
-      changeCelcius.classList.remove("link-like");
-      changeFharenheit.classList.add("link-like");
-      currentDegree.innerHTML = Math.round(response.data.main.temp);
-    }
-
-    changeFharenheit.addEventListener("click", convertFharenheit);
-    function convertFharenheit(event) {
-      event.preventDefault();
-      changeFharenheit.classList.remove("link-like");
-      changeCelcius.classList.add("link-like");
-      currentDegree.innerHTML = Math.round(
-        response.data.main.temp * 1.8 + 32.0
-      );
-    }
-  }
-}
-//date
-
-let now = new Date();
-
-let currentDay = document.querySelector("#current-day");
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-currentDay.innerHTML = days[now.getDay()];
-let currentHour = document.querySelector("#current-hour");
-currentHour.innerHTML = `${now.getHours()} : ${now.getMinutes()}`;
-
-//search button
-
-let searchCity = document.querySelector("#search-city");
-searchCity.addEventListener("submit", chooseCity);
-
-function chooseCity(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#type-city");
-  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${units}`;
-  axios.get(apiURL).then(showTemperature);
-  function showTemperature(response) {
-    currentCityName.innerHTML = response.data.name;
-    currentDegree.innerHTML = Math.round(response.data.main.temp);
-    currentCondition.innerHTML = response.data.weather[0].description;
-    currentWind.innerHTML = `Wind: ${Math.round(
-      response.data.wind.speed
-    )} km/h`;
-    currentHumid.innerHTML = `Humidity: ${response.data.main.humidity}%`;
-    let CurrentEmoji = document.querySelector(`#current-emoji`);
-    CurrentEmoji.setAttribute(
-      "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-    CurrentEmoji.setAttribute("alt", `${response.data.weather[0].description}`);
-
-    let lon = response.data.coord.lon;
-    let lat = response.data.coord.lat;
-    let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
-    axios.get(forecastUrl).then(displayForecast);
-
-    // celsius to fharenheit
 
     changeFharenheit.classList.add("link-like");
     changeCelcius.classList.remove("link-like");
@@ -130,6 +73,7 @@ function chooseCity(event) {
     }
   }
 }
+
 //forecast
 
 function displayForecast() {
@@ -160,3 +104,20 @@ function displayForecast() {
 
   forecastElement.innerHTML = forecastHTML;
 }
+//date
+
+let now = new Date();
+
+let currentDay = document.querySelector("#current-day");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+currentDay.innerHTML = days[now.getDay()];
+let currentHour = document.querySelector("#current-hour");
+currentHour.innerHTML = `${now.getHours()} : ${now.getMinutes()}`;
